a9fbf5208af4ab958eab943bf278d387898a41f7
micheci <michecimartinez@gmail.com>
2025-05-06 16:27:44 -0500

    updated version to mke it look nicer

--- public/js/moneyLeft.js
+++ public/js/moneyLeft.js
@@ -1,16 +1,29 @@
-function getMoneyLeft(){
-  
-    let moneyLeft=document.getElementById('incomeMoney')
-    let spendingLeft=document.getElementById("spendingMoney");
-    console.log(spendingLeft)
-    
-    let total=Number(moneyLeft.innerHTML)- Number(spendingTotal.innerHTML)
-   
-    
-    document.getElementById("spendingLeft").innerHTML+=total;
+function getMoneyLeft() {
+  const incomeElem = document.getElementById("incomeMoney");
+  const spendingLeftElem = document.getElementById("spendingLeft");
+  const spendingElems = document.querySelectorAll(".wholeSpending");
 
-    
-    
- 
- }
- 
+  if (!incomeElem || !spendingLeftElem) {
+    console.error("Required elements not found in the DOM");
+    return;
+  }
+
+  const income = Number(incomeElem.innerText) || 0;
+
+  let totalSpending = 0;
+  spendingElems.forEach((el) => {
+    const value = Number(el.innerText);
+    if (!isNaN(value)) {
+      totalSpending += value;
+    }
+  });
+
+  // Update the spendingTotal display (optional, if you want to show it)
+  const spendingTotalElem = document.getElementById("spendingTotal");
+  if (spendingTotalElem) {
+    spendingTotalElem.innerText = totalSpending;
+  }
+
+  const moneyLeft = income - totalSpending;
+  spendingLeftElem.innerText = moneyLeft;
+}
--- public/js/total.js
+++ public/js/total.js
@@ -1,13 +1,11 @@
-function getResult(){
-   let income=document.getElementsByClassName("wholeSpending");
-   console.log(income)
-   let sum=0;
-   for(let i=0;i<income.length;i++){
-        sum+=Number(income[i].innerHTML)
-   }
-   
-   
-    document.getElementById("spendingTotal").innerHTML+=sum;
+function getResult() {
+  let income = document.getElementsByClassName("wholeSpending");
+  console.log(income, "elSpending");
 
+  let sum = 0;
+  for (let i = 0; i < income.length; i++) {
+    sum += Number(income[i].dataset.amount);
+  }
+
+  document.getElementById("spendingTotal").innerHTML = `${sum.toFixed(2)}`;
 }
-
